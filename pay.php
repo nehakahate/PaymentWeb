843205b6d11f202181f187f00173f5b1e414908b
nehakahate <neha.kahate991@gmail.com>
2020-12-18 14:57:00 +0530

    payment code

---
+++ pay.php
@@ -0,0 +1,31 @@
+<?php
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://test.instamojo.com/api/1.1/payment-requests/');
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array("X-Api-Key:test_8d0d7c8cc0ee3090ceb05a1a119",
+                  "X-Auth-Token:test_d1446ebd5b450721e696e7a2eeb"));
+$payload = Array(
+    'purpose' => 'Book Order',
+    'amount' => '10',
+    'phone' => '9999999999',
+    'buyer_name' => 'John Doe',
+    'redirect_url' => 'http://www.example.com/redirect/',
+    'send_email' => true,
+    'webhook' => 'http://www.example.com/webhook/',
+    'send_sms' => true,
+    'email' => 'nehak@gmail.com',
+    'allow_repeated_payments' => false
+);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+$response = curl_exec($ch);
+curl_close($ch); 
+
+echo $response;
+
+?>
